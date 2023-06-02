8a37c57dfae44dd1b6062aad99c561b8b6d7f4fe
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-02 02:00:30 -0400

    Update: removed react fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { P5AudioVisualizer } from './components/p5.js/P5AudioVisualization';
 await import('p5/lib/addons/p5.sound');
 
 function App() {
-  return (
-    <>
-      <P5AudioVisualizer />
-    </>
-  );
+  return <P5AudioVisualizer />;
 }
 
 export default App;
